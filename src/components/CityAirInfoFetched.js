417e314fb5b8f5d377091aa17e29807e177bb106
OxanaSF <ovtyutcheva@gmail.com>
2022-07-06 06:01:38 -0700

    retrieved fetched data in CityAirInfoFetched.js component

--- src/components/CityAirInfoFetched.js
+++ src/components/CityAirInfoFetched.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-
-
 const CityAirInfoFetched = (props) => {
-
-
   const [cityWeatherInfo, setCityWeatherInfo] = useState([]);
+  const [date, setDate] = useState('');
+  const [aqius, setaQius] = useState(0);
+  const [img, setImg] = useState('')
+
   let params = useParams();
 
   const getCityWeatherInfo = (cityName) => {
@@ -19,12 +19,17 @@ const CityAirInfoFetched = (props) => {
       params: {
         q: cityName,
       },
-     
     };
     axios
       .request(options)
       .then((response) => {
-        setCityWeatherInfo(response.data.results);
+        // console.log('Cities: ', response.data)
+        setCityWeatherInfo(response.data.data.cities[0]);
+        setDate(
+          response.data.data.cities[0].currentMeasurement.ts.slice(0, 10)
+        );
+        setaQius(response.data.data.cities[0].currentMeasurement.aqius);
+        setImg(response.data.data.news[0].contributors.picture)
       })
       .catch((error) => {
         console.error(error);
@@ -38,11 +43,14 @@ const CityAirInfoFetched = (props) => {
 
   return (
     <CityAirInfoFetchedStyled>
-      <h3>{params.name} air info</h3>
+      {/* <h3>{params.name} air info</h3> */}
 
-  
-      
-
+      <div>
+        {cityWeatherInfo.city}, {cityWeatherInfo.country},{' '}
+      </div>
+      <div>{date}</div>
+      <div>{aqius}</div>
+      <img src={img} alt="" />
     </CityAirInfoFetchedStyled>
   );
 };
