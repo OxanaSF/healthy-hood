1deb3270e36a18655574ef9545fd2f50e6b3b5c0
johnson <johnsonle2929@gmail.com>
2022-07-06 08:43:57 -0700

    testing axios on fitness

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 const PORT = 8000;
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json('hi, I am backend');
+app.get("/", (req, res) => {
+  res.json("hi, I am backend");
 });
 
 // API Request - search by a city name to get Air info for the city
 
-app.get('/search', (req, res) => {
+app.get("/search", (req, res) => {
   const city = req.query.q;
 
-  console.log('**********************');
-  console.log('**********************');
-  console.log('**********************');
-  console.log('BACK END CITY:', city);
-  console.log('**********************');
-  console.log('**********************');
-  console.log('**********************');
+  console.log("**********************");
+  console.log("**********************");
+  console.log("**********************");
+  console.log("BACK END CITY:", city);
+  console.log("**********************");
+  console.log("**********************");
+  console.log("**********************");
 
-
-  
   const options = {
-    method: 'GET',
-    url: 'https://airvisual1.p.rapidapi.com/v2/auto-complete',
+    method: "GET",
+    url: "https://airvisual1.p.rapidapi.com/v2/auto-complete",
     params: { q: city },
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-      'X-RapidAPI-Host': 'airvisual1.p.rapidapi.com',
+      "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+      "X-RapidAPI-Host": "airvisual1.p.rapidapi.com",
     },
   };
 
@@ -44,6 +42,30 @@ app.get('/search', (req, res) => {
       res.json(response.data);
     })
     .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.get("/bodypart", (req, res) => {
+  const muscle = req.query.bodypart;
+
+  const options = {
+    method: "GET",
+    url: "https://exercisedb.p.rapidapi.com/exercises/name/%7Bname%7D",
+    params: { bodypart: muscle },
+
+    headers: {
+      "X-RapidAPI-Key": "e951adc5f4msh6b381ff752324a3p1d054ajsn22a2c583f766",
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
       console.error(error);
     });
 });
