c1183681a862af16708eec2a1df13732ca33d4f0
Mike <mikegv@users.noreply.github.com>
2022-07-08 16:20:16 -0700

    Mike nutritionspage (#21)
    * updated nutrition page
    
    * added searchNutritionValue.js and RecipeVideos.js
    
    * changed pages to add routes for nutrition
    
    * added styling to pages, added folder Nutrition

--- src/components/Nutrition/RecipeVideos.js
+++ src/components/Nutrition/RecipeVideos.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 import { useState } from 'react'
 import styled from 'styled-components'
+import ClockLoader from 'react-spinners/ClockLoader';
+import { ResultItem, StyledButton, VideoPlayerContainer, ResultsArea } from './StyledComponents'
+
 
 const FormStyled = styled.form`
   padding-top: 5rem;
@@ -31,30 +34,30 @@ const FormStyled = styled.form`
     letter-spacing: 1px;
   }
 `;
-// const DUMMY_DATA = [{
-//     title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
-//     youTubeId: "2h0bhpqFKpM",
-//     rating: 1,
-//     views: 379,
-//     thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
-//     length: 476
-// },
-// {
-//     title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
-//     youTubeId: "dfaj4vI8QxE",
-//     rating: 1,
-//     views: 379,
-//     thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
-//     length: 476
-// },
-// {
-//     title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
-//     youTubeId: "wdvruTuWvW8",
-//     rating: 1,
-//     views: 379,
-//     thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
-//     length: 476
-// }]
+const DUMMY_DATA = [{
+    title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
+    youTubeId: "2h0bhpqFKpM",
+    rating: 1,
+    views: 379,
+    thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
+    length: 476
+},
+{
+    title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
+    youTubeId: "dfaj4vI8QxE",
+    rating: 1,
+    views: 379,
+    thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
+    length: 476
+},
+{
+    title: "how to make chicken soup - dinner recipes - homemade from scratch - stock recipes -",
+    youTubeId: "wdvruTuWvW8",
+    rating: 1,
+    views: 379,
+    thumbnail: "https://i.ytimg.com/vi/2h0bhpqFKpM/mqdefault.jpg",
+    length: 476
+}]
 
 
 
@@ -65,6 +68,7 @@ const RecipeVideos = () => {
     const [showVideo, setShowVideo] = useState(false)
     const [videoId, setVideoId] = useState('')
 
+    const [loading, setLoading] = useState(false)
 
 
     const getVideos = (value) => {
@@ -76,7 +80,7 @@ const RecipeVideos = () => {
                 query: value,
                 minLength: '0',
                 maxLength: '999',
-                number: '10',
+                number: '15',
                 offset: '0'
             },
             headers: {
@@ -89,10 +93,12 @@ const RecipeVideos = () => {
         axios.request(options).then(function (response) {
             console.log(response.data);
             console.log(response.data.videos)
+            setLoading(false)
             setResults(response.data.videos)
             setSearchValue('')
         }).catch(function (error) {
             console.error(error)
+            setLoading(false)
         });
 
 
@@ -108,8 +114,8 @@ const RecipeVideos = () => {
 
     const submitHandler = (e) => {
         e.preventDefault()
-
         if (searchValue !== '') {
+            setLoading(true)
             getVideos(searchValue)
         }
     }
@@ -126,31 +132,44 @@ const RecipeVideos = () => {
                 <FormStyled name='videos' onSubmit={submitHandler}>
                     <label>Search by food:</label>
                     <input type='text' placeholder='chicken soup' onChange={changeHandler} />
-                    <button>Submit</button>
+                    <StyledButton>Submit</StyledButton>
                 </FormStyled>
-                <br />
+                <ResultsArea>
                 {results.length > 0 && <p>Total Results  {results.length} </p>}
+                {loading && <p>Loading...</p>}
+
                 <br />
-                {results.length > 0 && results.map(result => <><img src={result.thumbnail} onClick={() => openVideo(result.youTubeId)} /><br /></>)}
+                {
+                    loading ? 
+                    <ClockLoader
+                        loading={loading}
+                        size={150}
+                        color='orange'
+                        display="flex"
+                        /> :
+                 results.length > 0 &&
+                 results.map(result => <ResultItem key={result.youTubeId}><img src={result.thumbnail} onClick={() => openVideo(result.youTubeId)} /></ResultItem>)
+                }
 
                 <br />
                 <br />
+                </ResultsArea>
             </div>
         );
     } else {
         return (
-            <div>
+            <VideoPlayerContainer>
                 <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`}
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen="allowfullscreen"
-                    mozallowfullscreen="mozallowfullscreen" 
-                    msallowfullscreen="msallowfullscreen" 
-                    oallowfullscreen="oallowfullscreen" 
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen="allowfullscreen"
+                    mozallowfullscreen="mozallowfullscreen"
+                    msallowfullscreen="msallowfullscreen"
+                    oallowfullscreen="oallowfullscreen"
                     webkitallowfullscreen="webkitallowfullscreen">
                 </iframe>
-                <button onClick={() => setShowVideo(false)}>Close Video</button>
-            </div>
+                <StyledButton onClick={() => setShowVideo(false)}>Close Video</StyledButton>
+            </VideoPlayerContainer>
         )
     }
 
--- src/components/Nutrition/SearchNutritionValue.js
+++ src/components/Nutrition/SearchNutritionValue.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 import { useState } from 'react'
 import styled from 'styled-components'
-
+import ClockLoader from 'react-spinners/ClockLoader';
+import { StyledButton, ResultsArea, ResultItem } from './StyledComponents'
 const FormStyled = styled.form`
   padding-top: 5rem;
   margin: auto;
@@ -677,245 +678,243 @@ const DUMMY_RECIPE_INFO = {
 
 
 
-const DUMMY_DATA =[
-{
-    "id": 4748,
-    "title": "Grilled Salmon with Roasted Corn Relish",
-    "image": "https://spoonacular.com/recipeImages/4748-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 310,
-    "protein": "36g",
-    "fat": "12g",
-    "carbs": "16g"
-},
-{
-    "id": 5935,
-    "title": "Roast Shrimp With Fennel, Bacon & Orange",
-    "image": "https://spoonacular.com/recipeImages/5935-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 260,
-    "protein": "30g",
-    "fat": "12g",
-    "carbs": "7g"
-},
-{
-    "id": 12533,
-    "title": "Sausage And Chicken Jambalaya",
-    "image": "https://spoonacular.com/recipeImages/12533-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 324,
-    "protein": "26g",
-    "fat": "10g",
-    "carbs": "31g"
-},
-{
-    "id": 695218,
-    "title": "Apple-&-Fennel Roasted Pork Tenderloin",
-    "image": "https://spoonacular.com/recipeImages/695218-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 244,
-    "protein": "25g",
-    "fat": "6g",
-    "carbs": "22g"
-},
-{
-    "id": 707751,
-    "title": "Jasmine Rice-Stuffed Peppers",
-    "image": "https://spoonacular.com/recipeImages/707751-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 441,
-    "protein": "35g",
-    "fat": "18g",
-    "carbs": "36g"
-},
-{
-    "id": 707890,
-    "title": "Cherry, Cashew, and Pork Sandwich",
-    "image": "https://spoonacular.com/recipeImages/707890-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 327,
-    "protein": "22g",
-    "fat": "9g",
-    "carbs": "40g"
-},
-{
-    "id": 717910,
-    "title": "Slow Cooker Coconut Lemon Chicken",
-    "image": "https://spoonacular.com/recipeImages/717910-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 316,
-    "protein": "37g",
-    "fat": "15g",
-    "carbs": "8g"
-},
-{
-    "id": 825820,
-    "title": "Bacon Mushroom Stuffed Chicken",
-    "image": "https://spoonacular.com/recipeImages/825820-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 448,
-    "protein": "41g",
-    "fat": "20g",
-    "carbs": "27g"
-},
-{
-    "id": 872915,
-    "title": "Fiesta Chicken Pasta Bake",
-    "image": "https://spoonacular.com/recipeImages/872915-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 424,
-    "protein": "25g",
-    "fat": "18g",
-    "carbs": "41g"
-},
-{
-    "id": 874765,
-    "title": "Skinny Chicken & Broccoli Alfredo",
-    "image": "https://spoonacular.com/recipeImages/874765-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 360,
-    "protein": "35g",
-    "fat": "17g",
-    "carbs": "17g"
-},
-{
-    "id": 1122249,
-    "title": "Grilled Lamb Kebabs with Basil Tahini Sauce",
-    "image": "https://spoonacular.com/recipeImages/1122249-312x231.jpg",
-    "imageType": "jpg",
-    "calories": 435,
-    "protein": "22g",
-    "fat": "36g",
-    "carbs": "6g"
-}
+const DUMMY_DATA = [
+    {
+        "id": 4748,
+        "title": "Grilled Salmon with Roasted Corn Relish",
+        "image": "https://spoonacular.com/recipeImages/4748-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 310,
+        "protein": "36g",
+        "fat": "12g",
+        "carbs": "16g"
+    },
+    {
+        "id": 5935,
+        "title": "Roast Shrimp With Fennel, Bacon & Orange",
+        "image": "https://spoonacular.com/recipeImages/5935-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 260,
+        "protein": "30g",
+        "fat": "12g",
+        "carbs": "7g"
+    },
+    {
+        "id": 12533,
+        "title": "Sausage And Chicken Jambalaya",
+        "image": "https://spoonacular.com/recipeImages/12533-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 324,
+        "protein": "26g",
+        "fat": "10g",
+        "carbs": "31g"
+    },
+    {
+        "id": 695218,
+        "title": "Apple-&-Fennel Roasted Pork Tenderloin",
+        "image": "https://spoonacular.com/recipeImages/695218-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 244,
+        "protein": "25g",
+        "fat": "6g",
+        "carbs": "22g"
+    },
+    {
+        "id": 707751,
+        "title": "Jasmine Rice-Stuffed Peppers",
+        "image": "https://spoonacular.com/recipeImages/707751-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 441,
+        "protein": "35g",
+        "fat": "18g",
+        "carbs": "36g"
+    },
+    {
+        "id": 707890,
+        "title": "Cherry, Cashew, and Pork Sandwich",
+        "image": "https://spoonacular.com/recipeImages/707890-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 327,
+        "protein": "22g",
+        "fat": "9g",
+        "carbs": "40g"
+    },
+    {
+        "id": 717910,
+        "title": "Slow Cooker Coconut Lemon Chicken",
+        "image": "https://spoonacular.com/recipeImages/717910-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 316,
+        "protein": "37g",
+        "fat": "15g",
+        "carbs": "8g"
+    },
+    {
+        "id": 825820,
+        "title": "Bacon Mushroom Stuffed Chicken",
+        "image": "https://spoonacular.com/recipeImages/825820-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 448,
+        "protein": "41g",
+        "fat": "20g",
+        "carbs": "27g"
+    },
+    {
+        "id": 872915,
+        "title": "Fiesta Chicken Pasta Bake",
+        "image": "https://spoonacular.com/recipeImages/872915-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 424,
+        "protein": "25g",
+        "fat": "18g",
+        "carbs": "41g"
+    },
+    {
+        "id": 874765,
+        "title": "Skinny Chicken & Broccoli Alfredo",
+        "image": "https://spoonacular.com/recipeImages/874765-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 360,
+        "protein": "35g",
+        "fat": "17g",
+        "carbs": "17g"
+    },
+    {
+        "id": 1122249,
+        "title": "Grilled Lamb Kebabs with Basil Tahini Sauce",
+        "image": "https://spoonacular.com/recipeImages/1122249-312x231.jpg",
+        "imageType": "jpg",
+        "calories": 435,
+        "protein": "22g",
+        "fat": "36g",
+        "carbs": "6g"
+    }
 ]
 
 
 const SearchNutritionValue = () => {
     const [results, setResults] = useState([])
     const [loading, setLoading] = useState(false)
-  const [nutritionVals, setNutritionVals] = useState({ minProtein: 0, maxCals: 0, maxCarbs: 0 })
+    const [nutritionVals, setNutritionVals] = useState({ minProtein: 0, maxCals: 0, maxCarbs: 0 })
 
-  const nutritionChangeHandler = (e) => {
-    console.log(e.target.name)
-    let temp = { ...nutritionVals }
-    temp[e.target.name] = e.target.value
-    console.log(temp)
-    setNutritionVals({ ...temp })
-  }
-
-  
-  const submitHandler = (e) => {
-   e.preventDefault()
-   fetchByNutrition()
-}
-
-  const fetchByNutrition = () => {
-
-    console.log('nutrition')
-
-
-    const options = {
-      method: 'GET',
-      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients',
-      params: {
-        limitLicense: 'false',
-        minProtein: nutritionVals.minProtein,
-        maxCalories: nutritionVals.maxCalories,
-        offset: '0',
-        maxCarbs: nutritionVals.maxCarbs,
-        number: '50'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY_3,
-        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-      }
-    };
-
-    axios.request(options).then(function (response) {
-      console.log('response....')
-      console.log(response.data)
-    setResults(response.data)
-
-    }).catch(function (error) {
-      console.error(error)
-    })
-
-
-  }
-
-
-
-
-
-
-const recipeInfo = (id) => {
-
-  const options = {
-    method: 'GET',
-    url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
-    headers: {
-      'X-RapidAPI-Key': 'b52d740b0bmsh46c29c8b8293547p1818aejsn80a3b0fe26c0',
-      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    const nutritionChangeHandler = (e) => {
+        console.log(e.target.name)
+        let temp = { ...nutritionVals }
+        temp[e.target.name] = e.target.value
+        console.log(temp)
+        setNutritionVals({ ...temp })
     }
-  };
-  
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-  }).catch(function (error) {
-      console.error(error);
-  });
-
-}
 
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        fetchByNutrition()
+    }
+
+    const fetchByNutrition = () => {
+
+        console.log('nutrition')
+
+
+        const options = {
+            method: 'GET',
+            url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByNutrients',
+            params: {
+                limitLicense: 'false',
+                minProtein: nutritionVals.minProtein,
+                maxCalories: nutritionVals.maxCalories,
+                offset: '0',
+                maxCarbs: nutritionVals.maxCarbs,
+                number: '50'
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY_3,
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            console.log('response....')
+            console.log(response.data)
+            setLoading(false)
+            setResults(response.data)
+
+        }).catch(function (error) {
+            console.error(error)
+            setLoading(false)
+        })
+
+    }
 
 
 
 
+    const recipeInfo = (id) => {
 
+        const options = {
+            method: 'GET',
+            url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_API_KEY_3,
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+            }
+        };
 
+        // axios.request(options).then(function (response) {
+        //     console.log(response.data);
+        // }).catch(function (error) {
+        //     console.error(error);
+        // });
 
-
+    }
 
 
 
 
     return (
         <div>
-            
+
 
             Search for recipes by nutrition content <br />
-    <FormStyled name='byNutrition' onSubmit={submitHandler}>
+            <FormStyled name='byNutrition' onSubmit={submitHandler}>
 
-      <label>Minimum Protein in grams:</label>
-      <input name='minProtein' type='number' min='10' step='5' value={nutritionVals.minProtein} onChange={nutritionChangeHandler} required/>
-      <label>Maximum Calories:</label>
-      <input name='maxCals' type='number' min='0' step='25' value={nutritionVals.maxCals} onChange={nutritionChangeHandler} required/>
-      <label>Maximum Carbs</label>
-      <input name='maxCarbs' type='number' min='0' step='10' value={nutritionVals.maxCarbs} onChange={nutritionChangeHandler} required />
+                <label>Minimum Protein in grams:</label>
+                <input name='minProtein' type='number' min='10' step='5' value={nutritionVals.minProtein} onChange={nutritionChangeHandler} required />
+                <label>Maximum Calories:</label>
+                <input name='maxCals' type='number' min='0' step='25' value={nutritionVals.maxCals} onChange={nutritionChangeHandler} required />
+                <label>Maximum Carbs</label>
+                <input name='maxCarbs' type='number' min='0' step='10' value={nutritionVals.maxCarbs} onChange={nutritionChangeHandler} required />
 
-      <button>Submit</button>
-    </FormStyled>
-    <br />
-    {loading && <p>Loading...</p>}
-    <br />
-    {results.map(item => 
-    <>
-      <p key={item.id}>
-        
-        <h3>{item.title}</h3>
-        <img src={item.image} onClick={()=>recipeInfo(item.id)} />
-        
-        <br />
-        <span>{`${item.calories} calories ${item.protein} protein ${item.carbs} carbs ${item.fat} fat`}</span>
-      </p>
-      <br />
-      <br />
-      </>)
-    
-          }
-    <br />
+                <StyledButton>Submit</StyledButton>
+            </FormStyled>
+            <ResultsArea>
+            {loading && <p>Loading...</p>}
+            <br />
+            {
+                loading ?
+                    <ClockLoader
+                        loading={loading}
+                        size={150}
+                        color='orange'
+                        display="flex"
+                    /> :
+                    results.length > 0 &&
+                    results.map(item =>
+                        <ResultItem key={item.id}>
+
+                                <h3>{item.title}</h3>
+                                <img src={item.image} onClick={() => recipeInfo(item.id)} />
+
+                                <br />
+                                <span>{`${item.calories} calories ${item.protein} protein ${item.carbs} carbs ${item.fat} fat`}</span>
+                            
+                            <br />
+                            <br />
+                        </ResultItem>)
+
+            }
+            </ResultsArea>
         </div>
     );
 };
