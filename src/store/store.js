9fb9a905f6e06e88348acfbcc293643353f8d032
mikegv <xmik3yvx@live.com>
2022-07-12 13:43:00 -0700

    added authSlice to store, connected store with RecipeVideos.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,31 @@ const favoritesSlice = createSlice({
     name: 'favorites',
     initialState: {favoritesList: []},
     reducers: {
-        
         addToFavorites(state, action) {
-            state.favoritesList.push({name: action.payload})
+            const item = action.payload
+            console.log('item: ', item)
+            state.favoritesList.push({...item})
+        }
+    }
+})
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {loggedIn: true},
+    reducers: {
+        toggleLoggedIn(state) {
+            state.loggedIn = !state.loggedIn
         }
     }
 })
 
 const store = configureStore({
-    reducer: favoritesSlice.reducer
+    reducer: {
+        favorites: favoritesSlice.reducer,
+        auth: authSlice.reducer
+    }
 })
 
 export const favoritesActions = favoritesSlice.actions
-
+export const authActions = authSlice.actions
 export default store
