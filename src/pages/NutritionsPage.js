b1f750f0da44a146e3dfd03426121f94cf911f1f
Mike <mikegv@users.noreply.github.com>
2022-07-13 09:23:38 -0700

    Redux on RecipeVideos (#35)
    * updated nutrition page
    
    * added searchNutritionValue.js and RecipeVideos.js
    
    * changed pages to add routes for nutrition
    
    * added styling to pages, added folder Nutrition
    
    * added RecipeDetails.js and api call
    
    * small style changes
    
    * added redux toolkit and react redux
    
    * added store, updated index.js, added lines to RecipeVideos.js
    
    * added authSlice to store, connected store with RecipeVideos.js
    
    * added Profile.js
    
    * fixed files
    
    * Added working links for saved items to Profile and RecipeVideos

--- src/pages/NutritionsPage.js
+++ src/pages/NutritionsPage.js
@@ -1,12 +1,10 @@
 import styled from "styled-components"
 import { Link,Outlet } from "react-router-dom"
 
-
 const NutritionsPage = () => {
 
   return (
     <NutritionsPageStyled>
-      
       <h2>Find A Recipe For Your Healthy Lifestyle Today</h2>
       <div>
         <Link to='recipe-videos' className='nutritionLink'>Find A Video Of A Recipe</Link>
