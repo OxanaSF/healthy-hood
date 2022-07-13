dfc71be8559633907214c166c7e9fadabf540c41
johnson <johnsonle2929@gmail.com>
2022-07-13 13:48:03 -0700

    updated styles

--- src/components/FitnessSection/FitnessSearch.js
+++ src/components/FitnessSection/FitnessSearch.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useState, useEffect, useCallback } from "react";
 import AnimatedPage from "../../animations/AnimatedPageTransition";
 import ExerciseList from "./ExerciseList";
+import { animateScroll as scroll } from "react-scroll";
 
 const FitnessSelect = (props) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -51,11 +52,14 @@ const FitnessSelect = (props) => {
   }, [fetchExercises]);
 
   let content = <p>You have not selected and exercise.</p>;
-
+  const scrollToBottom = () => {
+    scroll.scrollToBottom();
+  };
   const handleChange = (e) => {
     e.preventDefault();
     setIsLoading(true);
     setExercise(items[e.target.value]);
+    scrollToBottom();
   };
 
   if (isLoading) {
@@ -104,12 +108,6 @@ const SelectSection = styled.section`
     border: 3px solid rgb(254, 233, 218);
     border-radius: 3rem;
     width: fit-content;
-  }
-
-  img {
-    border: 10px solid rgb(254, 233, 218);
-    border-radius: 3rem;
-    margin: 2rem auto;
   }
 `;
 
--- src/pages/FitnessPage.js
+++ src/pages/FitnessPage.js
@@ -1,3 +1,4 @@
+import { calculateSize } from "@iconify/react";
 import styled from "styled-components";
 import AnimatedPageTransition from "../animations/AnimatedPageTransition";
 import FitnessSelect from "../components/FitnessSection/FitnessSearch";
@@ -22,12 +23,25 @@ const FitnessPage = () => {
             requires your body to burn calories. There are many types of
             physical activity, including swimming, running, jogging, walking,
             and dancing, to name a few. Being active has been shown to have many
-            health benefits, both physically and mentally. It may even help you
-            live longer.
+            health benefits, both physically and mentally.{" "}
+            <b>It may even help you live longer.</b>
           </p>
         </div>
-
-        <h2>Choose an Exercise</h2>
+        <div
+          className="select-fitness"
+          style={{
+            backgroundImage: `url(${
+              process.env.PUBLIC_URL + "/images/fitness.png"
+            })`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            borderRadius: "20px",
+            margin: "0 10vw",
+            height: "30vh",
+          }}
+        >
+          <h2 className="select">Choose an Exercise</h2>
+        </div>
         <div className="fitness-dropdown">
           <FitnessSelect />
         </div>
@@ -37,23 +51,29 @@ const FitnessPage = () => {
 };
 
 const FitnessPageStyled = styled.section`
+  color: #8c777c;
   h1 {
     font-size: 4.5rem;
     font-weight: 600;
-    color: #243966;
     margin: 3rem 0;
     text-align: center;
   }
   .fitness-benefits {
     margin: 0 10rem 1rem 10rem;
-    color: #243966;
     font-size: 1.6rem;
     text-align: center;
+  }
+  .select {
+    font-size: 2rem;
+    position: relative;
+    top: 30px;
+    font-weight: 600;
   }
   h2 {
     margin-top: 2rem;
     text-align: center;
     font-size: 2rem;
+    color: black;
   }
 `;
 
