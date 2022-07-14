c20a6ea74ffdafe5fa34f705264f7f1c43b83c84
OxanaSF <ovtyutcheva@gmail.com>
2022-07-14 09:34:02 -0700

    fixed merge conflict

--- src/pages/AirQualityPage.js
+++ src/pages/AirQualityPage.js
@@ -4,7 +4,7 @@ import { NavLink, Outlet } from 'react-router-dom';
 
 import { motion } from 'framer-motion';
 
-import { motion } from "framer-motion";
+
 import {
   slidesFromLeftLeaveToLeft,
   slidesFromRightftLeaveToRight,
